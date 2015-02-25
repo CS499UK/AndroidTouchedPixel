58ce6ad54d7704c379174b29e8be98307a966c40
Bryan Potere <bpotere@gmail.com>
2015-02-25 11:40:42 -0500

    Merge branch 'master' of /home/bryan/AndroidStudioProjects/AndroidTouchedPixel with conflicts.

---
+++ lib/src/main/java/com/example/TesseractExample.java
@@ -0,0 +1,25 @@
+package com.example;
+/**
+ * Created by bryan on 2/9/15.
+ */
+
+import net.sourceforge.tess4j.*;
+
+
+import java.io.File;
+
+public class TesseractExample {
+
+    public static void example(String[] args) {
+        File imageFile = new File("eurotext.tif");
+        Tesseract instance = Tesseract.getInstance();  // JNA Interface Mapping
+        // Tesseract1 instance = new Tesseract1(); // JNA Direct Mapping
+
+        try {
+            String result = instance.doOCR(imageFile);
+            System.out.println(result);
+        } catch (TesseractException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+}
